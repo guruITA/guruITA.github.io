ba0f5b83e70b2e99be7dae3f47ca5db22a84a23b
Gurpreet Singh <Singhgurpreet14082002@gmail.com>
2026-09-21 13:03:22 +0200

    style: add Home module with SCSS styling for layout and content

---
+++ src/componets/sections/Home/Home.tsx
@@ -0,0 +1,12 @@
+import styles from './Home.module.scss';
+
+function Home() {
+    return (
+        <section className={styles.home}>
+            <h1>Gurpreet Singh</h1>
+            <p>Software Engineering Student & Full-Stack Developer</p>
+        </section>
+    );
+}
+
+export default Home;
